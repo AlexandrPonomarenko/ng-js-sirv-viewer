f1b8f099a8a4d8f2728478f0e8dc92303b1d8731
Alexandr <furriets@gmail.com>
2022-06-10 13:06:49 +0300

    update conf files, remove some cong files

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,7 @@
-export * from './src/app/app.module';
+
+export * from './projects/sirv-media-viewer/src/lib/interfaces/SMV';
+export * from './projects/sirv-media-viewer/src/lib/interfaces/SMVSlider';
+export * from './projects/sirv-media-viewer/src/lib/interfaces/SMVEvents';
+export * from './projects/sirv-media-viewer/src/lib/interfaces/SMVOptions';
+export * from './projects/sirv-media-viewer/src/lib/sirv-media-viewer.component';
+export * from './projects/sirv-media-viewer/src/lib/sirv-media-viewer.module';
